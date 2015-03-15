8b5bc11f086318d7d93ba7191bdbb04bd08eccb1
Zoffix Znet <cpan@zoffix.com>
2015-03-14 22:37:07 -0400

    Contact form and other stuff

--- XTaTIK.pl
+++ XTaTIK.pl
@@ -13,7 +13,29 @@ helper xtext => sub {
 
 get '/' => 'index';
 
-get $_ for qw(/products /about /contact);
+get $_ for qw(/products /about  /contact);
+
+post '/contact' => sub {
+    my $c = shift;
+
+    # Check CSRF token
+    my $val= $c->validation;
+    return $c->render(text => 'Bad CSRF token!', status => 403)
+        if $val->csrf_protect->has_error('csrf_token');
+
+    $val->required('name')->size(1, 200);
+    $val->required('email')->size(1, 200);
+    $val->required('province')->in(
+        qw/AB BC MB NB NF NT NS NU ON PE QC SK YT/
+    );
+
+    $c->render(text => "Low orbit ion cannon pointed at ")
+        unless $val->has_error;
+
+        say for $val->error;
+
+    return $c->render( template => 'contact' );
+};
 
 app->start;
 
