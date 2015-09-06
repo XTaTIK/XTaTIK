1820c96f0fe60fb520ad68410a8a87b9e06b5f58
Zoffix Znet <cpan@zoffix.com>
2015-09-06 18:05:49 -0400

    Properly display no-pic files on search results (Closes #144)

--- lib/XTaTIK/Controller/Search.pm
+++ lib/XTaTIK/Controller/Search.pm
@@ -4,18 +4,29 @@ package XTaTIK::Controller::Search;
 
 use Mojo::Base 'Mojolicious::Controller';
 use XTaTIK::Common qw/n_to_br/;
+use File::Spec::Functions qw/catfile/;
 
 sub search {
     my $self = shift;
 
+    my @prods = $self->products->get_by_id(
+        $self->product_search->search( $self->param('term') )
+    );
+
+    $self->_find_product_pic( $_->{image} ) for @prods;
+
     $self->stash(
         template => 'root/search',
-        products => [
-            $self->products->get_by_id(
-                $self->product_search->search( $self->param('term') )
-            )
-        ],
+        products => \@prods,
     );
+}
+
+sub _find_product_pic {
+    my $self = shift;
+
+    $_[0] = 'nopic.png'
+        unless $self->app->static
+            ->file( catfile 'product-pics', $_[0]//'' );
 }
 
 1;
