bc57febd0953a40b4c76be8571dc71da24d1e200
Zoffix Znet <cpan@zoffix.com>
2015-09-07 15:52:55 -0400

    Prerelease

--- lib/XTaTIK.pm
+++ lib/XTaTIK.pm
@@ -41,7 +41,7 @@ sub startup {
         unshift @sass_path,
             catdir $ENV{XTATIK_COMPANY}, 'public', 'sass';
 
-        use lib catdir $ENV{XTATIK_COMPANY}, 'lib';
+        eval 'use lib ' . catdir $ENV{XTATIK_COMPANY}, 'lib';
     }
 
     unshift @sass_path,
@@ -65,7 +65,7 @@ sub startup {
     unshift @sass_path,
         catdir $silo_path, 'public', 'sass';
 
-    use lib catdir $silo_path, 'lib';
+    eval 'use lib ' . catdir $silo_path, 'lib';
 
     unshift @sass_path,
             catdir rel2abs(curdir), qw/lib XTaTIK public  sass  fake-site/
